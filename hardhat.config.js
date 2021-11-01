eb570ca98185890316f867d64655abf3143457fc
hanhvn <vonhathanh@gmail.com>
2021-11-01 09:51:55 +0700

    add bnft contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,46 @@
-require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
 module.exports = {
-  solidity: "0.8.4",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        bsctest: {
+            url: "https://data-seed-prebsc-2-s3.binance.org:8545/",
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        bscmainnet: {
+            url: "https://bsc-dataseed1.defibit.io/",
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    solidity: {
+        version: "0.8.1",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+    mocha: {
+        timeout: 20000,
+    },
 };
